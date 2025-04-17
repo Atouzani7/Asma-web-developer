72ec71e9960c8ecd2a67bd14b8dfe39d0190ed20
Asma <asma.touzani7@gmail.com>
2025-04-17 04:42:04 +0200

    config bdd ok

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -6,10 +6,37 @@ import { FormationModule } from './formation/formation.module';
 import { ProfileModule } from './profile/profile.module';
 import { ProjectModule } from './project/project.module';
 import { SkillsModule } from './skills/skills.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { env } from 'process';
+import { Profile } from './profile/entities/profile.entity';
+import { Company } from './company/entities/company.entity';
+import { Formation } from './formation/entities/formation.entity';
+import { Project } from './project/entities/project.entity';
+import { Skill } from './skills/entities/skill.entity';
 
 @Module({
-  imports: [CompanyModule, FormationModule, ProfileModule, ProjectModule, SkillsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE || env.DB_DATABASE,
+      // autoLoadEntities: true,
+      synchronize: true,
+      entities: [Profile, Project, Company, Formation, Skill],
+    }),
+    ProfileModule,
+    SkillsModule,
+    ProjectModule,
+    CompanyModule,
+    FormationModule,
+  ],
+  // imports: [CompanyModule, FormationModule, ProfileModule, ProjectModule, SkillsModule],
+  // controllers: [AppController],
+  // providers: [AppService],
 })
 export class AppModule {}

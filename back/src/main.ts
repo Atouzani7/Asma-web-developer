53a171898cea95de20428adfc48101f4fce3b051
Asma <asma.touzani7@gmail.com>
2025-05-09 00:13:30 +0200

    url cors

--- back/src/main.ts
+++ back/src/main.ts
@@ -6,7 +6,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: 'https://asma-web-developer.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });

878db2a59111e0535538927d7bb2b406cf995274
Asma <asma.touzani7@gmail.com>
2025-05-08 20:27:09 +0200

    adding branch test

---
+++ frontend/src/components/Test.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function TestEnv() {
+    console.log("API URL:", process.env.NEXT_PUBLIC_API_URL); // Test
+
+    return (
+        <div>
+            <h1>Test des Variables d'Environnement</h1>
+            <p>API URL: {process.env.NEXT_PUBLIC_API_URL}</p>
+
+
+
+        </div>
+    );
+}
